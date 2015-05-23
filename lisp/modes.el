8e5254d7f6294da92c58882441bb25cf5e3cb6b3
Joe Kitchen <joe@grill>
2015-05-23 16:49:52 +0100

    More reorganisation of modes.el
    There should be no changes to functionality here, simply changes to
    organisation of the modes files.

--- lisp/modes.el
+++ lisp/modes.el
@@ -17,11 +17,47 @@
                (evil-set-initial-state 'magit-status-mode 'normal)
                (evil-set-initial-state 'magit-diff-mode 'normal)
                (evil-set-initial-state 'magit-log-mode 'normal)
+               ;; Advice taken from what the emacsd!?
+               (defadvice magit-status (around magit-fullscreen activate)
+                 "Open magit-status in fullscreen rather than splitting into two windows."
+                 (window-configuration-to-register :magit-fullscreen)
+                 ad-do-it
+                 (delete-other-windows))
+                (add-hook 'magit-log-edit-mode-hook 'refill-mode)
+               ;; Magit keybindings with evil mode
+               (evil-define-key 'normal magit-mode-map
+                 "j" 'magit-goto-next-section
+                 "k" 'magit-goto-previous-section
+                 "TAB" 'magit-toggle-section
+                 "g" 'magit-refresh
+                 "G" 'magit-refresh-all
+                 "s" 'magit-stage-item
+                 "S" 'magit-stage-all
+                 "?" 'magit-describe-item
+                 ":" 'magit-git-command
+                 "RET" 'magit-visit-item
+                 "t" 'magit-key-mode-popup-tagging
+                 "r" 'magit-key-mode-popup-rewriting
+                 "P" 'magit-key-mode-popup-pushing
+                 "f" 'magit-key-mode-popup-fetching
+                 "b" 'magit-key-mode-popup-branching
+                 "M" 'magit-key-mode-popup-remoting
+                 "B" 'magit-key-mode-popup-bisecting
+                 "F" 'magit-key-mode-popup-pulling
+                 "l" 'magit-key-mode-popup-logging
+                 "c" 'magit-log-edit
+                 "q" 'magit-quit-window
+                 "d" 'magit-diff-working-tree
+                 "D" 'magit-diff)
                ))
 
-(autoload 'rainbow-delimiters-mode "rainbow-delimiters")
+(use-package rainbow-delimiters
+  :ensure t
+  :config (progn
+            (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+            ))
 
-(electric-pair-mode)
+(electric-pair-mode 1)
 (column-number-mode 1)
 (menu-bar-mode 0)
 (global-linum-mode 1)
@@ -63,14 +99,6 @@
                 (if (looking-back "/")
                     (insert "~/")
                   (call-interactively 'self-insert-command))))))
-
-;; magit mode
-; taken from what the emacsd!?
-(defadvice magit-status (around magit-fullscreen activate)
-  "Open magit-status in fullscreen rather than splitting into two windows."
-  (window-configuration-to-register :magit-fullscreen)
-  ad-do-it
-  (delete-other-windows))
 
 ;; flyspell mode
 (defun my-save-word ()
@@ -119,7 +147,7 @@
 (defun my-prog-mode-hook ()
   "Run when entering any mode inheriting from prog-mode"
   (flyspell-prog-mode)
-  (rainbow-delimiters-mode)
+  ;(rainbow-delimiters-mode)
   (hl-line-mode))
 (add-hook 'prog-mode-hook 'my-prog-mode-hook)
 
@@ -138,7 +166,6 @@
 (add-hook 'tex-mode-hook (lambda () (set-input-method "british")))
 
 ; magit modes
-(add-hook 'magit-log-edit-mode-hook 'refill-mode)
 (add-hook 'magit-status-mode-hook (lambda () (evil-local-mode 1)))
 
 ; lisp modes
--- lisp/my-evil.el
+++ lisp/my-evil.el
@@ -20,117 +20,94 @@
              )
 
 (use-package evil
-             :ensure evil
-             :demand evil
-             :config (progn
-                       (evil-mode 1)
-                       )
-             )
+  :ensure evil
+  :demand evil
+  :config (progn
+            (evil-mode 1)
+            ;; Evil-mode advice
+            ;; Both advice for evil functions, and advice which only
+            ;; needs to take effect when evil mode is active goes in
+            ;; this section
+            (defadvice evil-goto-line (after evil-goto-line-and-center activate)
+              "When using the goto line command ('G'), try to center the line"
+              (evil-scroll-line-to-center (line-number-at-pos)))
+            (defadvice evil-ex-search-next (after evil-find-key-and-center activate)
+              "When using the search next command ('n'), try to center the line"
+              (evil-scroll-line-to-center (line-number-at-pos)))
+            (defadvice switch-to-buffer (before return-to-normal-now activate)
+              "In evil mode, return to normal state before switching buffer"
+              (when evil-mode
+                (evil-normal-state)))
+            (defadvice other-window (before other-window-normal-now activate)
+              "In evil mode, return to normal state before switching window"
+              (when evil-mode
+                (evil-normal-state)))
 
-
-(defun evil-yank-to-eol ()
-  "Yank to end-of-line in evil mode.  Useful for making the
+            ;; Evil-mode functions
+            (defun evil-yank-to-eol ()
+              "Yank to end-of-line in evil mode.  Useful for making the
   behaviour of Y sane, i.e. analagous to D, C etc., and not the
   same as yy."
-  (interactive)
-  (evil-yank (point) (point-at-eol)))
+              (interactive)
+              (evil-yank (point) (point-at-eol)))
 
-(defadvice evil-goto-line (after evil-goto-line-and-center activate)
-  "When using the goto line command ('G'), try to center the line"
-  (evil-scroll-line-to-center (line-number-at-pos)))
-
-(defadvice evil-ex-search-next (after evil-find-key-and-center activate)
-  "When using the search next command ('n'), try to center the line"
-  (evil-scroll-line-to-center (line-number-at-pos)))
-
-(defadvice switch-to-buffer (before return-to-normal-now activate)
-  "Return to normal mode before switching buffer"
-  (when evil-mode
-    (evil-normal-state)))
-
-(defadvice other-window (before other-window-normal-now activate)
-  "Return to normal mode before switching window"
-  (when evil-mode
-    (evil-normal-state)))
-
-; Evil mode keys
-(eval-after-load 'evil (progn
-                         (define-key evil-normal-state-map (kbd "<left>") 'switch-to-prev-buffer)
-                         (define-key evil-normal-state-map (kbd "<right>") 'switch-to-next-buffer)
-                         (define-key evil-normal-state-map "L" 'evil-end-of-line)
-                         (define-key evil-normal-state-map "H" 'smart-move-to-beginning-of-line)
-                         (define-key evil-normal-state-map "Y" 'evil-yank-to-eol)
-                         (define-key evil-normal-state-map "zg" 'my-save-word)
-                         (define-key evil-normal-state-map "]s" 'flyspell-goto-next-error)
-                         (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
-                         (define-key evil-visual-state-map (kbd "C-g") 'evil-normal-state)
-                         (define-key evil-replace-state-map (kbd "C-g") 'evil-normal-state)
-                         ))
-
-; Mode-specific evil keymaps
-
-(evil-define-key 'normal prog-mode-map
-  (kbd "C-c i") 'indent-buffer
-  )
-
-(evil-define-key 'visual prog-mode-map
-  (kbd "C-c i") 'indent-region
-  )
-
-(evil-define-key 'normal org-mode-map
-  (kbd "TAB") 'org-cycle
-  )
-
-(evil-define-key 'normal eww-mode-map
-  "q" 'kill-active-buffer
-  (kbd "DEL") 'eww-back-url
-  )
-
-(evil-define-key 'normal dired-mode-map
-  "l" 'dired-find-alternate-file
-  "h" 'my-dired-up-directory
-  "q" 'kill-this-buffer
-  "n" 'evil-search-next
-  "N" 'evil-search-previous
-  )
-
-(evil-define-key 'normal ibuffer-mode-map
-  "j" 'ibuffer-forward-line
-  "k" 'ibuffer-backward-line
-  "l" 'ibuffer-visit-buffer
-  "U" 'ibuffer-unmark-backward
-  "/" 'ibuffer-jump-to-buffer
-  )
-
-(evil-define-key 'emacs ibuffer-mode-map
-  "j" 'ibuffer-forward-line
-  "k" 'ibuffer-backward-line
-  "l" 'ibuffer-visit-buffer
-  "/" 'ibuffer-jump-to-buffer
-  )
-
-(evil-define-key 'normal magit-mode-map
-  "j" 'magit-goto-next-section
-  "k" 'magit-goto-previous-section
-  "TAB" 'magit-toggle-section
-  "g" 'magit-refresh
-  "G" 'magit-refresh-all
-  "s" 'magit-stage-item
-  "S" 'magit-stage-all
-  "?" 'magit-describe-item
-  ":" 'magit-git-command
-  "RET" 'magit-visit-item
-  "t" 'magit-key-mode-popup-tagging
-  "r" 'magit-key-mode-popup-rewriting
-  "P" 'magit-key-mode-popup-pushing
-  "f" 'magit-key-mode-popup-fetching
-  "b" 'magit-key-mode-popup-branching
-  "M" 'magit-key-mode-popup-remoting
-  "B" 'magit-key-mode-popup-bisecting
-  "F" 'magit-key-mode-popup-pulling
-  "l" 'magit-key-mode-popup-logging
-  "c" 'magit-log-edit
-  "q" 'magit-quit-window
-  "d" 'magit-diff-working-tree
-  "D" 'magit-diff
-  )
+            ;; Evil-mode keybindings
+            ;; Define evil keys for normal state:
+            (progn
+              (define-key evil-normal-state-map (kbd "<left>") 'switch-to-prev-buffer)
+              (define-key evil-normal-state-map (kbd "<right>") 'switch-to-next-buffer)
+              (define-key evil-normal-state-map "L" 'evil-end-of-line)
+              (define-key evil-normal-state-map "H" 'smart-move-to-beginning-of-line)
+              (define-key evil-normal-state-map "Y" 'evil-yank-to-eol)
+              (define-key evil-normal-state-map "zg" 'my-save-word)
+              (define-key evil-normal-state-map "]s" 'flyspell-goto-next-error))
+            ;; Define evil keys in insert state:
+            (progn
+              (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state))
+            ;; Define evil keys in visual state:
+            (progn
+              (define-key evil-visual-state-map (kbd "C-g") 'evil-normal-state))
+            ;; Define evil keys in replace state:
+            (progn
+              (define-key evil-replace-state-map (kbd "C-g") 'evil-normal-state))
+            ;; Define evil keys in normal state when prog-mode is active
+            (evil-define-key 'normal prog-mode-map
+              (kbd "C-c i") 'indent-buffer
+              )
+            ;; Define evil keys in visual state when prog-mode is active
+            (evil-define-key 'visual prog-mode-map
+              (kbd "C-c i") 'indent-region
+              )
+            ;; Define evil keys in normal state when org-mode is active
+            (evil-define-key 'normal org-mode-map
+              (kbd "TAB") 'org-cycle
+              )
+            ;; Define evil keys in normal state when eww-mode is active
+            (evil-define-key 'normal eww-mode-map
+              "q" 'kill-active-buffer
+              (kbd "DEL") 'eww-back-url
+              )
+            ;; Define evil keys in normal state when dired-mode is active
+            (evil-define-key 'normal dired-mode-map
+              "l" 'dired-find-alternate-file
+              "h" 'my-dired-up-directory
+              "q" 'kill-this-buffer
+              "n" 'evil-search-next
+              "N" 'evil-search-previous
+              )
+            ;; Define evil keys in normal state when ibuffer-mode is active
+            (evil-define-key 'normal ibuffer-mode-map
+              "j" 'ibuffer-forward-line
+              "k" 'ibuffer-backward-line
+              "l" 'ibuffer-visit-buffer
+              "U" 'ibuffer-unmark-backward
+              "/" 'ibuffer-jump-to-buffer
+              )
+            ;; Define evil keys in emacs state when ibuffer-mode is active
+            (evil-define-key 'emacs ibuffer-mode-map
+              "j" 'ibuffer-forward-line
+              "k" 'ibuffer-backward-line
+              "l" 'ibuffer-visit-buffer
+              "/" 'ibuffer-jump-to-buffer
+              )
+            ))
