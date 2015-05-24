1286165dda79a176d2b9b5e7266ea2a1534ef4b3
Joe Kitchen <joe@grill>
2015-05-24 13:12:01 +0100

    Cleaned up some fancy-modeline stuff:
    Significantly rewrote README.md:
    
    Slightly expanded on the introduction.
    
    Expanded on and corrected information on some features.
    
    Wrote a quick guide to how it works and configuration.
    
    Added a section on the unlicense
    
    Cleaned up some of fancy-modeline.el:
    
    Added comments to the setq mode-line-input and mode-line-active
    sections noting that they don't actually *work*.
    
    Changed the layout of the defface code so it actually fits inside 80
    columns...

--- fancy-modeline/README.md
+++ fancy-modeline/README.md
@@ -5,30 +5,38 @@
 Fancy-Modeline is a modeline inspired by Luke Maciak's vim statusline
 plugin, [neatstatus](github.com/maciakl/vim-neatstatus).  It aims to
 provide a simple, readable UI with the information that matters as an
-emacs user.  It is designed for use with evil-mode for emacs, though
-it can be used without it.
+emacs user.
+
+Fancy-Modeline is designed for use with evil-mode, and is primarily
+helpful in keeping track of which state (analagous to vim's modes)
+evil-mode is in.  In principle there is no reason why one could not
+use fancy-modeline without evil-mode, though.
 
 ## Features:
 
-* Evil State Indication: keeps track of whether the user is in normal,
-  insert, visual, replace, or emacs state
+* Evil State Indication: keeps track of evil-mode's state.  The state
+  indicator is coloured for easy reading, although in the case of
+  replace and insert state, the colour is the same.  The different
+  letter key (I vs. R) and the overwrite indicator (which see below)
+  serve to differentiate the two.
 
-* Active Window Indication: has a notification for the currently
-  active window
+* Active Window Indication: the unused portion of the modeline is
+  highlighted differently for active and inactive windows.
 
-* Buffer name
+* Buffer Name
 
-* Current major mode
+* Current Major Mode
 
-* Cursor position: line and column number, and percentage of file read
+* Cursor position: In the form [Percentage read: line number column number].
 
 * Modified indicator
 
-* Read-only indicator
+* Read-only indicator: when a file is read-only, the "[RO]" flag is
+  added to the end of the modeline.
 
-* Overwrite indicator
-
-* Indicator for current insert method
+* Overwrite indicator: when overwriting is active (for instance, in
+  evil's replace state), the OVR flag is shown.  When overwriting is
+  off, the flag instead reads INS, for "insert".
 
 ## Future Features:
 
@@ -49,3 +57,29 @@ Features which may be implemented in the future include:
     (require 'fancy-modeline)
 
 3. Profit
+
+## Configuration:
+
+Fancy-Modeline is (hopefully) sensibly configured.  The first 30-odd
+lines define faces for the modeline.  Between their names and
+docstrings, they should be fairly self-explanatory.  
+
+Then we define various parts of the modeline: my-modeline-position,
+for instance, is the section which tells us whereabouts in the
+document the cursor is.  If you want the modeline to show something
+else, it should be reasonably simple to write another similar section.
+
+Note here that the final two variables, my-modeline-active and
+my-modeline-input have somewhat inconsistent actions, and so I don't
+use them.  Some day, if I am sufficiently missing the information that
+they would provide, I will write something which replaces them.
+
+Finally, we tie it all together with setq-default mode-line-format.
+This is where you can change which indicators are displayed on the
+modeline, and which order they are in.  
+
+## UnLicense:
+
+This code is released into the public domain with the
+[unlicense](unlicense.org).  The text of the unlicense can be found in
+the file UNLICENSE.
--- fancy-modeline/fancy-modeline.el
+++ fancy-modeline/fancy-modeline.el
@@ -18,19 +18,36 @@
 
 ;;; Code:
 
-(defface mode-line-standard-face `((t (:foreground "white" :background "black"))) "White on black as standard modeline font")
-(defface mode-line-bold-face `((t (:foreground "white" :weight bold :background "black"))) "White bold on black for important information")
-
-(defface mode-line-position-face `((t (:foreground "black" :weight bold :background "white"))) "Black on white for position of cursor")
-
-(defface mode-line-insert-face `((t (:foreground "black" :background "red"))) "Red background for insert-state indicator")
-(defface mode-line-normal-face `((t (:foreground "black" :background "blue"))) "Blue background for normal-state-indicator")
-(defface mode-line-visual-face `((t (:foreground "black" :background "green"))) "Green background for visual-state-indicator")
-(defface mode-line-emacs-face `((t (:foreground "black" :background "orange"))) "Orange background for emacs-state-indicator")
-
-(defface mode-line-active-face `((t (:foreground "black" :background "cyan"))) "Black on cyan for active window")
-(defface mode-line-modified-face `((t (:foreground "black" :weight bold :background "magenta"))) "Black on magenta for modified indicator")
-(defface mode-line-readonly-face `((t (:foreground "black" :weight bold :background "violet"))) "Black on violet for RO indicator")
+(defface mode-line-standard-face
+  `((t (:foreground "white" :background "black")))
+  "White on black as standard modeline font")
+(defface mode-line-bold-face
+  `((t (:foreground "white" :weight bold :background "black")))
+  "White bold on black for important information")
+(defface mode-line-position-face
+  `((t (:foreground "black" :weight bold :background "white")))
+  "Black on white for position of cursor")
+(defface mode-line-insert-face
+  `((t (:foreground "black" :background "red")))
+  "Red background for insert-state indicator")
+(defface mode-line-normal-face
+  `((t (:foreground "black" :background "blue")))
+  "Blue background for normal-state-indicator")
+(defface mode-line-visual-face
+  `((t (:foreground "black" :background "green")))
+  "Green background for visual-state-indicator")
+(defface mode-line-emacs-face
+  `((t (:foreground "black" :background "orange")))
+  "Orange background for emacs-state-indicator")
+(defface mode-line-active-face
+  `((t (:foreground "black" :background "cyan")))
+  "Black on cyan for active window")
+(defface mode-line-modified-face
+  `((t (:foreground "black" :weight bold :background "magenta")))
+  "Black on magenta for modified indicator")
+(defface mode-line-readonly-face
+  `((t (:foreground "black" :weight bold :background "violet")))
+  "Black on violet for RO indicator")
 
 (set-face-background 'mode-line "white")
 (set-face-background 'mode-line-inactive "black")
@@ -50,7 +67,7 @@ http://reddit.com/r/emacs/2hhhvg"
 
 (setq my-mode-line-readonly
       '(:eval (when buffer-read-only
-                (propertize "[ RO ]" 'face 'mode-line-readonly-face))))
+                (propertize "[RO]" 'face 'mode-line-readonly-face))))
 
 (setq my-mode-line-modified
       '(:eval (when (buffer-modified-p)
@@ -72,29 +89,31 @@ http://reddit.com/r/emacs/2hhhvg"
                     ((evil-visual-state-p)  (propertize " V " 'face 'mode-line-visual-face))
                     )))
 
-
-(setq my-mode-line-active
-      '(:eval (when (window-active-p)
-                (propertize " A " 'face 'mode-line-active-face))))
-
-(setq my-mode-line-input
-     '(:eval (propertize (message current-input-method) 'face 'mode-line-standard-face)))
-
 (setq my-mode-line-overwrite
       '(:eval (if overwrite-mode
                   (propertize "OVR" 'face 'mode-line-emacs-face)
                 (propertize "INS" 'face 'mode-line-standard-face))))
 
+
+(setq my-mode-line-active
+; this is extremely buggy and doesn't work at all consistently
+      '(:eval (when (window-active-p)
+                (propertize " A " 'face 'mode-line-active-face))))
+
+(setq my-mode-line-input
+; this is extremely buggy and doesn't work at all consistently
+     '(:eval (propertize (message current-input-method) 'face 'mode-line-standard-face)))
+
 (setq-default mode-line-format
               (list
                my-mode-line-evilstate
-;               my-mode-line-active
                my-mode-line-buffname
                my-mode-line-modename
                my-mode-line-position
                my-mode-line-modified
                my-mode-line-readonly
                my-mode-line-overwrite
+;               my-mode-line-active
 ;               my-mode-line-input
                ))
 
