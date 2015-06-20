d832ec8f1a01c38814b1d130c1f5598f9e2f91fd
Joe Kitchen <joe@grill>
2015-06-20 21:01:16 +0100

    Wrote increment-integer-at-point

--- lisp/functions.el
+++ lisp/functions.el
@@ -228,3 +228,24 @@ From Nathan Typanski's blogpost 'Towards a vim-like emacs config'."
     (dired-up-directory)
     (kill-buffer old)
     ))
+
+(defun my-increment-integer-at-point (&optional arg)
+  "Increment the number forward from point by 'arg'."
+  (interactive "p*")
+  (save-excursion
+    (save-match-data
+      (let (inc-by field-width answer)
+        (setq inc-by (if arg arg 1))
+        (skip-chars-backward "[0-9]")
+        (when (re-search-forward "[0-9]+" nil t)
+          (setq field-width (- (match-end 0) (match-beginning 0)))
+          (setq answer (+ (string-to-number (match-string 0) 10) inc-by))
+          (when (< answer 0)
+            (setq answer (+ (expt 10 field-width) answer)))
+          (replace-match (format (concat "%0" (int-to-string field-width) "d")
+                                 answer)))))))
+
+(defun my-decrement-integer-at-point ()
+  "Decrement the number forward from point by 1."
+  (interactive)
+  (my-increment-integer-at-point -1))
